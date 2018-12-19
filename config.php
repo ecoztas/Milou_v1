8af6e72f0cef1d1aad98510b94a71ad5baabf791
Emre Can ÖZTAŞ <oztasemrecan@gmail.com>
2018-12-19 16:10:22 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,59 @@
+<?php
+defined('ROOT_PATH') or exit('ROOT_PATH is not found!');
+
+// -----------------------------------------------------------------------------
+// php.ini Settings.
+// -----------------------------------------------------------------------------
+error_reporting(E_ALL);
+ini_set('default_charset', 'utf-8');
+ini_set('memory_limit', -1);
+ini_set('max_execution_time', -1);
+ini_set('max_input_time', -1);
+ini_set('file_uploads', 1);
+ini_set('upload_max_filesize', '5M');
+ini_set('session.gc_maxlifetime', 14400);
+ini_set('date.timezone', 'Europe/Istanbul');
+ini_set('expose_php', -1);
+ini_set('allow_url_fopen', -1);
+ini_set('allow_url_include', -1);
+ini_set('disable_functions', 'exec, passthru, shell_exec, system, proc_open, popen, curl_exec, curl_multi_exec, parse_ini_file, show_source');
+gc_enable();
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// System settings (Database, HTTP Header, User Agent and Regex).
+// -----------------------------------------------------------------------------
+const SYSTEM_SETTINGS = array(
+	'data_file' => 'data.txt',
+	'database' => array(
+		'charset' => 'utf8',
+		'auth' => array(
+			'hostname' => 'localhost',
+			'username' => 'root',
+			'password' => ''
+		),
+		'info' => array(
+			'db_name'     => 'db_firma',
+			'db_table'    => 'tbl_firma',
+			'tbl_columns' => array('firma_adi', 'sehir', 'adres', 'telefon_1', 'telefon_2', 'website', 'url')
+		)
+	),
+	'http_header' => array(
+		'Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+		'Cache-Control: max-age=0',
+		'Connection: keep-alive',
+		'Keep-Alive: 300',
+		'Accept-Charset: ISO-8859-9,utf-8;q=0.7,*;q=0.7',
+		'Accept-Language: en-us,en;q=0.5',
+		'Pragma: '
+	),
+	'user_agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+	'reg_clear' => array(
+        '@<script[^>]*?>.*?</script>@si',   // Strip out javascript
+        '@<[\/\!]*?[^<>]*?>@si',            // Strip out HTML tags
+        '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
+        '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments
+	),
+	'reg_url' => '/^(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i'
+);
+// -----------------------------------------------------------------------------
---
+++ server.php
@@ -0,0 +1,227 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+
+/**
+ * @package     Milou_v1
+ * @author      Emre Can ÖZTAŞ (ecoz) <oztasemrecan@gmail.com>
+ * @copyright   Copyright (c) 2018, Emre Can ÖZTAŞ. (https://emrecanoztas.com/)
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://github.com/oztasemrecan/php-ping
+ * @since       Version 1.0.0
+ */
+
+// -----------------------------------------------------------------------------
+// Define constant.
+// -----------------------------------------------------------------------------
+defined('ROOT_PATH') or define('ROOT_PATH', realpath(__DIR__));
+defined('DIR_SEP') or define('DIR_SEP', '/');
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// Include file(s).
+// -----------------------------------------------------------------------------
+include(ROOT_PATH . DIR_SEP . 'config.php');
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// Define global variable(s).
+// -----------------------------------------------------------------------------
+// $found_url 		= array();
+// $visited_url 	= array();
+$page_details = array();
+$connection   = null;
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+// Main block.
+// -----------------------------------------------------------------------------
+if (php_sapi_name() === 'cli') {
+	$file = ROOT_PATH . DIR_SEP . SYSTEM_SETTINGS['data_file'];
+	if (file_exists($file)) {
+		$page = file_get_contents($file); // read data.txt
+		if (!empty($page)) {
+			$page_details = array_map('trim', explode(',', $page));
+            $base_url     = $page_details[0];
+            if (preg_match(SYSTEM_SETTINGS['reg_url'], $base_url)) {
+	            $const_url    = parse_url($base_url)['scheme'] . '://' . parse_url($base_url)['host'];
+				array_shift($page_details);
+				crawler($base_url, $const_url);
+            } else {
+            	exit('$base_url is not URL!');
+            }
+		} else {
+			exit(SYSTEM_SETTINGS['data_file'] . ' is empty!');
+		}
+	} else {
+		exit(SYSTEM_SETTINGS['data_file'] . ' is not found!');
+	}
+} else {
+	exit('This is for CLI programmers not for browserBoys!');
+}
+// -----------------------------------------------------------------------------
+
+/**
+ * -----------------------------------------------------------------------------
+ * Crawler method is crawling URL from web page.
+ * @param  string $base_url
+ * @param  string $const_url
+ * @return void
+ * -----------------------------------------------------------------------------
+ */
+function crawler($base_url, $const_url)
+{
+	static $found_url = array();
+	static $visited_url = array();
+
+	$curl = curl_init($base_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_USERAGENT, SYSTEM_SETTINGS['user_agent']);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, SYSTEM_SETTINGS['http_header']);
+	curl_setopt($curl, CURLOPT_ENCODING, '');
+	$html = curl_exec($curl);
+	curl_close($curl);
+
+	unset($curl);
+
+	$document = new DOMDocument();
+	libxml_use_internal_errors(true);
+	@$document->loadHTML($html);
+
+	$a_list = $document->getElementsByTagName('a');
+
+	if (is_object($a_list)) {
+		foreach ($a_list as $a) {
+			$a_href = $a->getAttribute('href');
+
+			if (substr($a_href, 0, 1) == "/" && substr($a_href, 0, 2) != "//") {
+				$a_href = parse_url($base_url)["scheme"] . "://" . parse_url($base_url)["host"] . $a_href;
+			} else if (substr($a_href, 0, 2) == "//") {
+				$a_href = parse_url($base_url)["scheme"] . ":" . $a_href;
+			} else if (substr($a_href, 0, 2) == "./") {
+				$a_href = parse_url($base_url)["scheme"] . "://" . parse_url($base_url)["host"] . dirname(parse_url($base_url)["path"]).substr($a_href, 1);
+			} else if (substr($a_href, 0, 1) == "#") {
+				$a_href = parse_url($base_url)["scheme"] . "://" . parse_url($base_url)["host"] . parse_url($base_url)["path"] . $a_href;
+			} else if (substr($a_href, 0, 3) == "../") {
+				$a_href = parse_url($base_url)["scheme"]."://".parse_url($base_url)["host"]."/".$a_href;
+			} else if (substr($a_href, 0, 11) == "javascript:") {
+				continue;
+			} else if (substr($a_href, 0, 5) != "https" && substr($a_href, 0, 4) != "http") {
+				$a_href = parse_url($base_url)["scheme"] . "://" . parse_url($base_url)["host"] . "/" . $a_href;
+			}
+
+			if (!in_array($a_href, $visited_url)) {
+				$parse_url = parse_url($a_href)['scheme'] . '://' . parse_url($a_href)['host'];
+				if ($const_url === $parse_url) {
+					$found_url[]   = $a_href;
+					$visited_url[] = $a_href;
+
+					scraper($a_href);
+				}
+			}
+		}
+	}
+
+	array_shift($found_url);
+
+	foreach ($found_url as $base_url) {
+		crawler($base_url, $const_url);
+	}
+}
+// -----------------------------------------------------------------------------
+
+/**
+ * -----------------------------------------------------------------------------
+ * Scraper method is scraping data from web page.
+ * @param  string $base_url
+ * @return void
+ * -----------------------------------------------------------------------------
+ */
+function scraper($base_url)
+{
+	global $page_details;
+	static $page_number = 0;
+
+	$page_content = array();
+
+	$curl = curl_init($base_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_USERAGENT, SYSTEM_SETTINGS['user_agent']);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, SYSTEM_SETTINGS['http_header']);
+	curl_setopt($curl, CURLOPT_ENCODING, '');
+	$html = curl_exec($curl);
+	curl_close($curl);
+
+	unset($curl);
+
+    $page_encoding = strtolower(mb_detect_encoding ($html));
+
+    if ($page_encoding != 'utf-8') {
+    	$html = mb_convert_encoding($html,'ISO-8859-1','utf-8');
+    }
+
+	libxml_use_internal_errors(true);
+	$document = new DOMDocument();
+	@$document->loadHTML($html);
+	$xpath = new DOMXPath($document);
+
+	echo(PHP_EOL . $base_url . PHP_EOL);
+
+	$control_data = @$xpath->query(trim($page_details[0]))->item(0)->textContent; // Data control: first field
+
+	if (!empty($control_data)) {
+		foreach ($page_details as $detail) {
+			$data = (string)trim(@$xpath->query(trim($detail))->item(0)->textContent);
+			$data = sanitize($data);
+			!empty(trim($data)) ? array_push($page_content, $data) : array_push($page_content, '-');
+		}
+
+		array_push($page_content, $base_url);
+		print_r($page_content);
+		database($page_content);
+	}
+}
+// -----------------------------------------------------------------------------
+
+function database($records)
+{
+	global $connection;
+
+	// ---> Connection control
+	if (!@mysqli_ping($connection)) {
+		$connection = mysqli_connect(
+			SYSTEM_SETTINGS['database']['auth']['hostname'],
+			SYSTEM_SETTINGS['database']['auth']['username'],
+			SYSTEM_SETTINGS['database']['auth']['password'],
+			SYSTEM_SETTINGS['database']['info']['db_name']
+		);
+
+		(mysqli_connect_errno()) ? exit('Connection failed!') : mysqli_set_charset($connection, SYSTEM_SETTINGS['database']['charset']);
+		mysqli_query($connection, "SET NAMES "  . SYSTEM_SETTINGS['database']['charset']);
+	}
+	// Connection control <---
+
+	$columns = implode(', ', SYSTEM_SETTINGS['database']['info']['tbl_columns']);
+	$records = '\'' . implode('\',' . '\'', $records) . '\'';
+
+	$query   = "INSERT INTO tbl_firma ($columns) VALUES ($records)";
+	$result  = mysqli_query($connection, $query);
+	
+	if (!$result) {
+		exit('Failed: ' . mysqli_error($connection));
+	}
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * Sanitize datas.
+ * @param  string $input
+ * @return string
+ * -----------------------------------------------------------------------------
+ */
+function sanitize($input)
+{
+	return(preg_replace(SYSTEM_SETTINGS['reg_clear'], '', $input));
+}
+// -----------------------------------------------------------------------------
